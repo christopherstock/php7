3b0b52c8835c8c34aede2183a9a4b53179700f56
Christopher Stock <email@christopherstock.de>
2016-04-22 16:03:58 +0200

    Tests the new null-coalescing-Operator.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     require_once( 'src/de/christopherstock/php7/Debug.php' );
     require_once( 'src/de/christopherstock/php7/DebugColor.php' );
-    require_once( 'src/de/christopherstock/php7/MyTestApp.php' );
+    require_once( 'src/de/christopherstock/php7/NewFeatures.php' );
 
     // render header
     echo '<head>';
@@ -10,17 +10,15 @@
     echo '</head>';
 
     //open HTML body
-    echo '<body style="background: #101010; font-family: verdana; color: #ff0000; margin: 25px; line-height: 175%;">';
+    echo '<body style="background: #101010; font-family: verdana, sans-serif; color: #ff0000; margin: 25px; line-height: 175%;">';
 
     //output a debug message
     Debug::out( DebugColor::GREEN, 'index.php being invoked' );
 
     //create a test class instance
-    $test1 = new MyTestApp();
-    MyTestApp::myStaticTestFunction( 2, "test string 1" );
+    $test1 = new NewFeatures();
+    NewFeatures::testNullCoalescingOperator( 2, "test string 1" );
     $test1->myTestMethod( 1, "test2" );
-
-    $test1->test( 1, "test2" );
 
     //close HTML body
     echo '</body>';
--- src/de/christopherstock/php7/NewFeatures.php
+++ src/de/christopherstock/php7/NewFeatures.php
@@ -3,14 +3,14 @@
     /***************************************************************
     *   A test class.
     ***************************************************************/
-    class MyTestApp
+    class NewFeatures
     {
         /***************************************************************
-        *   Being invoked when a new instance of MyTestApp is created.
+        *   Being invoked when a new instance of NewFeatures is created.
         ***************************************************************/
         public function __construct()
         {
-            Debug::out( DebugColor::GREEN, 'MyTestApp::construct() being invoked' );
+            Debug::out( DebugColor::GREEN, 'NewFeatures::construct() being invoked' );
         }
 
         /***************************************************************
@@ -21,19 +21,23 @@
         ***************************************************************/
         public static function myTestMethod( int $test1, string $test2)
         {
-            Debug::out( DebugColor::GREEN, 'MyTestApp::myTestMethod() being invoked [' . $test1 . '][' . $test2 . ']' );
+            Debug::out( DebugColor::GREEN, 'NewFeatures::myTestMethod() being invoked [' . $test1 . '][' . $test2 . ']' );
         }
 
         /***************************************************************
-        *   A static test function.
+        *   Tests the new null-coalescing-Operator.
         *
         *   @param  int    $test1
         *   @param  string $test2
         *   @return array
         ***************************************************************/
-        public static function myStaticTestFunction( int $test1, string $test2 ) : array
+        public static function testNullCoalescingOperator(int $test1, string $test2 ) : array
         {
-            Debug::out( DebugColor::BLUE, 'MyTestApp::myStaticTestFunction() being invoked [' . $test1 . '][' . $test2 . ']' );
+            Debug::out( DebugColor::GREEN, 'NewFeatures::testNullCoalescingOperator() being invoked [' . $test1 . '][' . $test2 . ']' );
+
+            $test4 = $undefinedVariable ?? 'myTest';
+
+            Debug::out( DebugColor::BLUE, 'Coalescing operation results to [' . $test4 . ']' );
 
             return array( 1, 2, 3, );
         }
